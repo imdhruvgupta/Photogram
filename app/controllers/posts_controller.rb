ca05b7925ad4fa0f7537e02a64a454e456cc8e0d
imdhruvgupta <gdhruv167@gmail.com>
2020-05-20 18:53:22 +0530

    create, read, update posts

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,34 @@
+class PostsController < ApplicationController
+    def index
+        @posts = Post.all
+    end
+
+    def create
+        @post = Post.new(post_params)
+
+        @post.save
+        redirect_to posts_path
+    end
+
+    def new
+    end
+
+    def edit
+        @post = Post.find(params[:id])
+    end
+
+    def update
+        @post = Post.find(params[:id])
+
+        if @post.update(post_params)
+            redirect_to posts_path
+        else
+            render 'edit'
+        end
+    end
+
+    private
+    def post_params
+        params.require(:post).permit(:image, :caption)
+    end
+end
